a3d0a31bbefc46c42bf269ea337dba2e034dbb46
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-04-05 19:48:34 -0700

    added some frontend so Jason will be happy

--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -1,23 +1,14 @@
 import React from 'react';
+import { Row, Input } from 'react-materialize';
 
+const Homepage = () => (
+  <Row>
+    <Input s={12} type="select" label="Materialize Select" defaultValue="2">
+      <option value="1">Option 1</option>
+      <option value="2">Option 2</option>
+      <option value="3">Option 3</option>
+    </Input>
+  </Row>
+);
 
-class Homepage extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      
-    }
-  }
-
-  componentDidMount() {
-    
-  }
-
-  render() {
-    return (
-      
-    );
-  }
-}
 export default Homepage;
--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -10,7 +9,6 @@ const users = require('./routes/users');
 
 const app = express();
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
