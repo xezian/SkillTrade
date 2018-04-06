b82dbd272b2cd8a1dd8d7ee4948cfacabdffd1e3
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-04-06 10:05:13 -0700

    Added posting route

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NoMatch from './pages/NoMatch';
-import { Banner } from './components/Banner';
+import Banner from './components/Banner';
+import HomePage from './pages/HomePage';
+import Posting from './pages/Posting';
 
 const App = () => (
   <Router>
     <div>
       <Banner />
       <Switch>
-        <Route exact path="/" component={Homepage} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/posting" component={Posting} />
         <Route component={NoMatch} />
       </Switch>
     </div>
