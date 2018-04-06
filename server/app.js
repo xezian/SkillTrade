bde7cf0531116c46148d86784273dcf01fff3f60
xezian <xezian@icloud.com>
2018-04-05 17:58:18 -0700

    fixed route issue, also removed serve-favicon and any reference to any such package

--- server/app.js
+++ server/app.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const index = require('./routes/index');
-const users = require('./routes/users');
 
 const app = express();
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,7 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -3,7 +3,7 @@ const needRoutes = require('./needs');
 const skillRoutes = require('./skills');
 const userRoutes = require('./users');
 
-// api routes
+// api routes (matches with /api/..)
 router.use('/needs/', needRoutes);
 router.use('/skills/', skillRoutes);
 router.use('/users/', userRoutes);
