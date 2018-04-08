09e519419513faec24e93552527fcd92fac61bea
xezian <xezian@icloud.com>
2018-04-07 21:40:24 -0700

    try againing

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', index);
 
 // for production only! then the client/build directory has it all.
 if (process.env.NODE_ENV === 'production') {
@@ -26,7 +26,6 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, '/client/build/index.html'));
   });
 }
-app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
