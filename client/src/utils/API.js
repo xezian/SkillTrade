79190b6de5eef323bba87c6281fa1181640725b6
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-04-08 17:07:03 -0700

    utils/API.js ready for authentication work

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export default {
+  // Based on the search input, results of skills or needs will be displayed
   getResults: (option, category) => {
     if (option === '1') {
       return axios.get(`/api/skills/${category}`);
@@ -10,4 +11,12 @@ export default {
     }
     return 'it is what it is';
   },
+
+  // Username will be matched to the password to verify the user and
+  // access to the users profile page will be given
+  getVerification: (username, password) => axios.post(`/api/users/${username}`, password),
+
+  // New user will be created
+  createUser: newUser => axios.post('/api/users', newUser),
 };
+
