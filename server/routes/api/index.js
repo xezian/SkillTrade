76ef747eb97185aa90a8311989456801c98b9582
xezian <xezian@icloud.com>
2018-04-09 22:32:22 -0700

    basic sign up all put together now

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -4,8 +4,8 @@ const skillRoutes = require('./skills');
 const userRoutes = require('./users');
 
 // api routes (matches with /api/..)
-router.use('/needs/', needRoutes);
-router.use('/skills/', skillRoutes);
-router.use('/users/', userRoutes);
+router.use('/needs', needRoutes);
+router.use('/skills', skillRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
