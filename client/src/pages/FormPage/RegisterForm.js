e6d9e417d7b145d3455546b9ab100dcc2223e967
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-04-12 11:14:00 -0700

    Added form validation error messages with styles

--- client/src/pages/FormPage/RegisterForm.js
+++ client/src/pages/FormPage/RegisterForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Row, Input, Button } from 'react-materialize';
 import API from '../../utils/API';
+import './Form.css';
 
 export class RegisterForm extends React.Component {
   constructor(props) {
@@ -12,6 +13,12 @@ export class RegisterForm extends React.Component {
       username: '',
       password: '',
       confirm: '',
+      firstNameError: '',
+      lastNameError: '',
+      emailError: '',
+      usernameError: '',
+      passwordError: '',
+      confirmError: '',
     };
 
     this.handleFirstChange = this.handleFirstChange.bind(this);
@@ -52,27 +59,45 @@ export class RegisterForm extends React.Component {
     // Firstname must consist of letters only
     if (!(/^[a-zA-Z\-]+$/).test(this.state.firstName)) {
       result = false;
+      this.setState({
+        firstNameError: 'Use letters only',
+      });
     }
     // Lastname must consist of letters only
     if (!(/^[a-zA-Z\-]+$/).test(this.state.lastName)) {
       result = false;
+      this.setState({
+        lastNameError: 'Use letters only',
+      });
     }
     // Email must match an email format
     if (!(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/).test(this.state.email)) {
       result = false;
+      this.setState({
+        emailError: 'Not a valid email address',
+      });
     }
     // Username must consist of letters and/or numbers
     if (!(/^[a-zA-Z0-9]+$/).test(this.state.username)) {
       result = false;
+      this.setState({
+        usernameError: 'Use letters and/or numbers only',
+      });
     }
     // Password must consist of at least:
     // 8 characters, 1 lowercase, 1 uppercase , 1 number, and 1 special character
     if (!(/^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,}$/).test(this.state.password)) {
       result = false;
+      this.setState({
+        passwordError: 'Not secure enough',
+      });
     }
     // Confirmation must match the password
     if (this.state.password !== this.state.confirm) {
       result = false;
+      this.setState({
+        confirmError: 'Passwords do not match',
+      });
     }
     return result;
   }
@@ -102,6 +127,12 @@ export class RegisterForm extends React.Component {
             username: '',
             password: '',
             confirm: '',
+            firstNameError: '',
+            lastNameError: '',
+            emailError: '',
+            usernameError: '',
+            passwordError: '',
+            confirmError: '',
           });
           console.log(res);
         })
@@ -112,46 +143,66 @@ export class RegisterForm extends React.Component {
   render() {
     return (
       <Row>
-        <Input
-          label="First Name"
-          s={6}
-          value={this.state.firstName}
-          onChange={this.handleFirstChange}
-        />
-        <Input
-          label="Last Name"
-          s={6}
-          value={this.state.lastName}
-          onChange={this.handleLastChange}
-        />
-        <Input
-          label="Email"
-          s={6}
-          value={this.state.email}
-          onChange={this.handleEmailChange}
-        />
-        <Input
-          label="Username"
-          s={6}
-          value={this.state.username}
-          onChange={this.handleUserChange}
-        />
-        <Input
-          label="Password"
-          s={6}
-          value={this.state.password}
-          onChange={this.handlePassChange}
-        />
-        <Input
-          label="Confirm Password"
-          s={6}
-          value={this.state.confirm}
-          onChange={this.handleConfirmChange}
-        />
+        <div className="input-div">
+          <Input
+            label="First Name"
+            s={12}
+            value={this.state.firstName}
+            onChange={this.handleFirstChange}
+          />
+          <div className="error-msg">{this.state.firstNameError}</div>
+        </div>
+        <div className="input-div">
+          <Input
+            label="Last Name"
+            s={12}
+            value={this.state.lastName}
+            onChange={this.handleLastChange}
+          />
+          <div className="error-msg">{this.state.lastNameError}</div>
+        </div>
+        <div className="input-div">
+          <Input
+            label="Email"
+            s={12}
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+          />
+          <div className="error-msg">{this.state.emailError}</div>
+        </div>
+        <div className="input-div">
+          <Input
+            label="Username"
+            s={12}
+            value={this.state.username}
+            onChange={this.handleUserChange}
+          />
+          <div className="error-msg">{this.state.usernameError}</div>
+        </div>
+        <div className="input-div">
+          <Input
+            label="Password"
+            s={12}
+            value={this.state.password}
+            onChange={this.handlePassChange}
+          />
+          <div className="error-msg">{this.state.passwordError}</div>
+        </div>
+        <div className="input-div">
+          <Input
+            label="Confirm Password"
+            s={12}
+            value={this.state.confirm}
+            onChange={this.handleConfirmChange}
+          />
+          <div className="error-msg">{this.state.confirmError}</div>
+        </div>
         <Button
-          disabled={!this.state.email || !this.state.username ||
+          disabled={!this.state.firstName || !this.state.lastName ||
+                    !this.state.email || !this.state.username ||
                     !this.state.password || !this.state.confirm}
           onClick={this.handleSubmit}
+          className="btn-form"
         >
           Create
         </Button>
