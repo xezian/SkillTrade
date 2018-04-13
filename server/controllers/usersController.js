7454d761f79338e4dfda61121a62a6c9d22f8231
xezian <xezian@icloud.com>
2018-04-12 21:20:02 -0700

    actually this change means you need to use a unique username and email address on signup

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -7,11 +7,6 @@ const passport = require('passport');
 
 // export functions to handle db interaction for users
 module.exports = {
-  checkUsername: (req, res) => {
-    db.User.count({ name: req.params.name })
-      .then(count => res.json(count))
-      .catch(err => res.status(422).json(err));
-  },
   login: (req, res, next) => {
     passport.authenticate('local', (err, user, info) => {
       if (err) { return next(err); }
@@ -20,24 +15,39 @@ module.exports = {
     })(req, res, next);
   },
   create: (req, res) => {
-    console.log('whoopie!');
-    // make salt
-    const salt = bcrypt.genSaltSync(10);
-    // get the data we need to encrypt the password
-    const password = req.body.password;
-    const hashWord = bcrypt.hashSync(password, salt);
-    // now make the nooUser Object we'll send to the db
-    const nooUser = {
-      username: req.body.username,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      hash: hashWord,
-      salt: salt,
-    };
-    db.User
-      .create(nooUser).save()
-      .then(instance => res.json(instance))
-      .catch(err => console.error(err));
+    db.User.count({ username: req.body.username })
+      .then((count) => {
+        if (count === 0) {
+          db.User.count({ email: req.body.email })
+            .then((count2) => {
+              if (count2 === 0) {
+                // make salt
+                const salt = bcrypt.genSaltSync(10);
+                // get the data we need to encrypt the password
+                const password = req.body.password;
+                const hashWord = bcrypt.hashSync(password, salt);
+                // now make the nooUser Object we'll send to the db
+                const nooUser = {
+                  username: req.body.username,
+                  firstName: req.body.firstName,
+                  lastName: req.body.lastName,
+                  email: req.body.email,
+                  hash: hashWord,
+                  salt: salt,
+                };
+                db.User
+                  .create(nooUser).save()
+                  .then(instance => res.json(instance))
+                  .catch(err => console.dir(err));
+              } else {
+                res.json('Sorry! That email is unavailable');
+              }
+            })
+            .catch(err => res.status(422).json(err));
+        } else {
+          res.json('Sorry! That username is unavailable');
+        }
+      })
+      .catch(err => res.status(422).json(err));
   },
 };
