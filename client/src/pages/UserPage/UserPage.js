6549d7f3455c7d9ebc9c87394b9a296e00e339c2
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-04-18 14:03:40 -0700

    userpage is ready to use data to display

---
+++ client/src/pages/UserPage/UserPage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import API from '../../utils/API';
+
+class UserPage extends React.Component {
+	state = {
+		user: this.props.match.params.username,
+		data: [],
+	}
+
+	componentDidMount = () => {
+    API.getUserData(this.state.user)
+      .then(res => {
+        this.setState({
+          data: res.data,
+        });
+        console.log(this.state.data)
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+	render() {
+		return (
+      <div>
+        <h2>Welcome {this.state.data.firstName}</h2>
+      </div>
+		);
+	}
+}
+
+export default UserPage;
