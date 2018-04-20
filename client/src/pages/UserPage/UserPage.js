80eac0c0372604542590e6043ac1130704805196
Roch Mirabeau <roch.mirabeau@gmail.com>
2018-04-19 19:32:02 -0700

    console log submitted skill/need

--- client/src/pages/UserPage/UserPage.js
+++ client/src/pages/UserPage/UserPage.js
@@ -9,6 +9,10 @@ class UserPage extends React.Component {
 		user: this.props.username,
 		data: [],
     selectedImage: null,
+    type: null,
+    category: null,
+    description: null,
+    title: null,
 	}
 
 	componentDidMount = () => {
@@ -41,6 +45,46 @@ class UserPage extends React.Component {
       .catch(err => {
         console.log(err);
       });
+  }
+
+  handleOptionChange = event => {
+    this.setState({
+      category: event.target.value
+    })
+    console.log(this.state)
+  }
+
+  handleTypeChange = event => {
+    this.setState({
+      type: event.target.value
+    })
+    console.log(this.state)
+  }
+
+  handleDescriptionChange = event => {
+    console.log(event)
+    this.setState({
+     description: event.target.value
+    })
+  }
+
+  handleTitleChange = event => {
+    console.log(this)
+    console.log(event)
+    this.setState({
+      title: event.target.value
+    }) 
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    console.log(this)
+    const postData = {}
+    postData.category = this.state.category
+    postData.type = this.state.type
+    postData.description = this.state.description
+    postData.title = this.state.title
+    console.log(postData)
   }
 
 	render() {
@@ -76,8 +120,8 @@ class UserPage extends React.Component {
               <Col s={12} style={{ height: 500, paddingLeft: 30 }}>
                 <h5 style={{ padding: 5, borderBottom: '1px solid #000' }}>Submit your skills or needs</h5>
                 <Row className="userpage-form">
-                  <Input name='group1' type='radio' value='skill' label='Skill' />
-                  <Input name='group1' type='radio' value='need' label='Need' />
+                  <Input name='group1' type='radio' value='Skill' label='Skill' onChange={this.handleTypeChange} />
+                  <Input name='group1' type='radio' value='Need' label='Need' onChange={this.handleTypeChange} />
                 </Row>
                 <Row className="userpage-form">
                   <Input
@@ -102,15 +146,19 @@ class UserPage extends React.Component {
                     s={12}
                     label="Title"
                     type="textarea"
+                    value={this.state.title}
+                    onChange={this.handleTitleChange.bind(this)}
                   />
                   <Input
                     s={12}
                     label="Description of your skill or need"
                     type="textarea"
+                    value={this.state.description}
+                    onChange={this.handleDescriptionChange.bind(this)}
                   />
                 </Row>
                 <Row>
-                  <Button className="userpage-btn">Submit</Button>
+                  <Button onClick={this.handleSubmit} className="userpage-btn">Submit</Button>
                 </Row>
               </Col>
             </Row>
